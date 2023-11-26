c9c687a9f97fc79692460d627d53b108c9def171
YashNagare <yashpnagare@gmail.com>
2023-11-26 23:13:02 +0530

    Added png/gif/vector assets

---
+++ app/src/main/java/com/practice/recipesapp/HomeActivity.kt
@@ -0,0 +1,15 @@
+package com.practice.recipesapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+    }
+}
