648d15a6dae2f4005ec7e6da4b4dc3c36c1e5b2f
YashNagare <yashpnagare@gmail.com>
2023-11-27 23:51:43 +0530

    Added room database dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,16 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
 }
 
 android {
     namespace = "com.practice.recipesapp"
     compileSdk = 34
+
+    buildFeatures {
+        viewBinding = true
+    }
 
     defaultConfig {
         applicationId = "com.practice.recipesapp"
@@ -45,4 +50,14 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("pl.droidsonroids.gif:android-gif-drawable:1.2.28")
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+
+    var room_version = "2.6.0"
+
+    implementation("androidx.room:room-runtime:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
+
+    // To use Kotlin annotation processing tool (kapt)
+    kapt("androidx.room:room-compiler:$room_version")
+
 }
